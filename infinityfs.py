b32c38de2f551fc283709135d265ce7743669fdb
Antoine Veenstra <antoineveenstra@gmail.com>
2014-10-17 02:09:34 +0200

    Some improvements

--- infinityfs.py
+++ infinityfs.py
@@ -4,23 +4,10 @@ from __future__ import with_statement
 
 from errno import EACCES
 from sys import argv, exit
-import time
+import os
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 
 maxpathlength = 2000
-now = time.time()
-st = {}
-st['st_mode']   = 16877
-st['st_ino']    = 0
-st['st_dev']    = 0
-st['st_nlink']  = 1
-st['st_uid']    = 1000
-st['st_gid']    = 1000
-st['st_size']   = 4096
-st['st_atime']  = now
-st['st_mtime']  = now
-st['st_ctime']  = now
-st['st_blocks'] = (int) ((st['st_size'] + 511) / 512)
 
 allchars = [
         str(chr(x)) for x in range(ord('A'),ord('Z')+1)
@@ -33,19 +20,23 @@ allchars = [
 folders = ['.','..'] + allchars
 
 class InfinityFS(LoggingMixIn, Operations):
-    def __init__(self):
-        pass
+    def __init__(self,path,f):
+        stos = os.lstat(path)
+        self.st = dict((key, getattr(stos, key)) for key in ('st_atime', 'st_ctime',
+            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+        stv = os.statvfs(path)
+        self.stv = dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
+            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
+            'f_frsize', 'f_namemax'))
+        self.file = f
+        stf = os.lstat(f)
+        self.fileAttr = dict((key, getattr(stf, key)) for key in ('st_atime', 'st_ctime',
+            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
     def access(self, path, mode):
         #print("access of path: \"%s\"" % path)
         if maxpathlength < len(path):
             raise FuseOSError(EACCES)
-
-    chmod = None
-    chown = None
-    create = None
-    flush = None
-    fsync = None
 
     def getattr(self, path, fh=None):
         #print("getattr of path: \"%s\"" % path)
@@ -53,39 +44,31 @@ class InfinityFS(LoggingMixIn, Operations):
             #print("  error")
             raise FuseOsError(ENOENT)
         #print("  returning st")
-        return st
+        if maxpathlength - 17 < len(path):
+            return self.fileAttr
+        return self.st
 
-    getxattr = None
-    link = None
-    listxattr = None
-    mkdir = None
-    mknod = None
-    open = None
-    read = None
+    def read(self, path, size, offset, fh):
+        f = os.open(self.file,os.O_RDONLY)
+        os.lseek(f,offset, 0)
+        buf = os.read(f,size)
+        os.close(f)
+        return buf
 
     def readdir(self, path, fh):
         #print("readdir of path: \"%s\"" % path)
         if maxpathlength < len(path):
             raise FuseOsError(ENOENT)
-        if maxpathlength < len(path) - 2:
+        if maxpathlength - 17 < len(path):
             return ['.','..']
+        if maxpathlength - 20 < len(path):
+            return ['.','..','script.py']
         return folders
-
-    readlink = None
-    release = None
-    rename = None
-    rmdir = None
-    statfs = None
-    symlink = None
-    truncate = None
-    unlink = None
-    utimens = None
-
 
 if __name__ == '__main__':
     if len(argv) != 2:
         print('usage: %s <mountpoint>' % argv[0])
         exit(1)
 
-    fuse = FUSE(InfinityFS(), argv[1], foreground=True)
+    fuse = FUSE(InfinityFS(argv[1],argv[0]), argv[1], foreground=True, allow_other=True)
 
